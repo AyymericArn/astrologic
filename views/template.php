0b04695cda3577841d0c79ffd75561e12e7493c1
ConstanceDvn <devconstance@gmail.com>
2019-04-09 16:28:44 +0200

    fix css

--- views/template.php
+++ views/template.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Blog</title>
-    <style> * {display: block;}</style>
+    <title></title>
+    <link href="https://fonts.googleapis.com/css?family=Capriola|Montserrat" rel="stylesheet">
     <link rel="stylesheet" href="./public/css/main.css">
 </head>
 
