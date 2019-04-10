32aab0ebe854b8dfba488ba8b75b79e2c272f9dc
AlexHillairet <alex.hillairet@hotmail.fr>
2019-04-10 11:38:27 +0200

    home front-end

--- views/pages/home.php
+++ views/pages/home.php
@@ -11,10 +11,26 @@ if($_SESSION['success']) {
 <?php }}
 $_SESSION['success'] = [];
 
+/*
 require('partials/horoscope.php');
 require('partials/cocktail.php');
 require('partials/recipe.php');
 require('partials/movie.php');
-
+*/
 ?>
-<div>Horoscope</div>
+<h2>Horoscope</h2>
+
+<span id="horoscope">
+    <img id="zodiacSign" src="public/img/zodiac-taurus.svg"  alt="Taurus sign"/>
+    <div id="zodiacTitle"> Taurus </div>
+    <img id="plusSign" src="public/img/+.png"  alt="+"/>
+    <div id="horoscopeDescription">Luck and prosperity lie within your grasp now, Aquarius. The only problem is that you might not notice because you're so caught up in some emotional drama that occupies...</div>
+</span>
+
+<h2>Calendar</h2>
+
+<div class="calendar">
+    <div> 19 </div>
+    <div> January </div>
+    <img id="plusSign" src="public/img/+.png"  alt="+"/>
+</div>
