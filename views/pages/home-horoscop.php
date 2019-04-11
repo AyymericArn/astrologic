3baae96d9adec0197eacd86d23aa93b68312197f
ConstanceDvn <devconstance@gmail.com>
2019-04-11 14:52:46 +0200

    horoscope + cards

--- views/pages/home-horoscop.php
+++ views/pages/home-horoscop.php
@@ -1,7 +1,10 @@
+<!-- menu -->
 <div class="header_home">
     <img class="menu_home" src="public/img/menu.svg" alt="">
     <img class="logo_home" src="public/img/astrologic.png" alt="">
 </div>
+
+<!-- horoscope -->
 <h1>Horoscope</h1>
 <div class="horoscope">
   <div class="header_horoscope">
@@ -14,19 +17,83 @@
   <div class="text_horoscope">
     <p>Luck and prosperity lie within your grasp now, Aquarius. The only problem is that you might not notice because you're so caught up in some emotional drama that occupies...</p>
   </div>
+
+  <!-- add infos onclick -->
+  <div class="text_horoscope horoscope_more">
+    <p>Luck and prosperity lie within your grasp now, Aquarius. The only problem is that you might not notice because you're so caught up in some emotional drama that occupies...</p>
+    <div class="horoscope_rating">
+      <p>75</p>    
+      <div class="health">Health</div>
+    </div>
+  </div>
 </div>
-<!-- <div class="horoscope">
-    <div class="header_horoscope">
-        <div class="header_horoscope_title">
-            <img src="public/img/zodiac-aquarius.svg" alt="zodiac sign aquarius">
-            <h3>Horoscope</h3>
-        </div>
-        <img class="plus_home" src="public/img/plus.svg" alt="">
+
+<!-- movie -->
+<div class="your_movie">
+  <div class="header_movie">
+    <h1>Your Movie</h1>
+    <img src="public/img/star.svg" alt="">
+  </div>
+  <div class="content_movie">
+    <img src="public/img/camping.jpg" alt="">
+    <div class="title_movie">
+      <div class="title_movie_header">
+        <h3>Camping</h3>
+        <p>2013</p>
+      </div>
+      <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, cum amet? Alias dolorum fuga aspernatur odio saepe.</p>
+      <a href="">Watch Movie</a>
     </div>
-    <div class="horoscope_text">
-        <p>Luck and prosperity lie within your grasp now, Aquarius. The only problem is that you might not notice because you're so caught up in some emotional drama that occupies...</p>
+  </div>
+</div>
+
+<!-- cocktail -->
+<div class="your_cocktail">
+  <div class="header_cocktail">
+    <h1>Your Cocktail</h1>
+    <img src="public/img/star.svg" alt="">
+  </div>
+  <div class="content_cocktail">
+    <img src="public/img/mojito.jpg" alt="">
+    <div class="title_cocktail">
+      <div class="title_cocktail_header">
+        <h3>Mojito</h3>
+      </div>
+      <ul class="ingredients_cocktail">
+        <li>Rhum</li>
+        <li>Mint</li>
+        <li>Lime</li>
+        <li>Sugar</li>
+        <li>Eau qui pique</li>
+      </ul>
+      <p class="see_recipe">+</p>
     </div>
-</div> -->
+  </div>
+  <div class="hidden_recipe">
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est harum officia inventore voluptate iusto dolore nostrum facilis molestias officiis dolores, placeat perferendis sequi, aut numquam cumque, iste incidunt tempora vitae.</p>
+  </div>
+</div>
+
+<!-- recipe -->
+<div class="your_recipe">
+  <div class="header_recipe">
+    <h1>Your recipe</h1>
+    <img src="public/img/star.svg" alt="">
+  </div>
+  <div class="content_recipe">
+    <img src="public/img/vrai_couscous.jpg" alt="">
+    <div class="title_recipe">
+      <div class="title_recipe_header">
+        <h3>Couscous</h3>
+        <p>750kcal</p>
+      </div>
+      <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, cum amet? Alias dolorum fuga aspernatur odio saepe.</p>
+      <a href="">See recipe</a>
+    </div>
+  </div>
+</div>
+
+<!-- calendar  -->
 <h1>Calendar</h1>
 <div class="vignette">
   <div class="show">
@@ -47,7 +114,7 @@
     </div>
     <div class="info-row info-row-3">
         <p class="recipe">Recipe</p>
-        <p class="recipe_name">Camping</p>
+        <p class="recipe_name">Couscous</p>
     </div>
   </div>
 </div>
